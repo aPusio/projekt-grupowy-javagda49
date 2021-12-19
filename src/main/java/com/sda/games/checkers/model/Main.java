966cfb3c1ea528295d9dffc7403111fa8631943d
Marcin Dolega <marcin92d@gmail.com>
2021-12-19 15:28:36 +0100

    Update - New branch for user io created

--- src/main/java/com/sda/games/checkers/model/Main.java
+++ src/main/java/com/sda/games/checkers/model/Main.java
@@ -1,17 +1,8 @@
 package com.sda.games.checkers.model;
 
-import com.sda.games.checkers.model.game.Game;
-
-import java.util.Scanner;
-
 public class Main {
 
-    private static Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws Exception {
-
-        Game game = new Game();
-        game.runGame();
+    public static void main(String[] args) {
 
     }
 }
