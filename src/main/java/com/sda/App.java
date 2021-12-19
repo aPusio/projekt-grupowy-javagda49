c087e7d4705be392ab6abede698f37db72b10f6d
ohanke <steamoha@wp.pl>
2021-12-19 11:19:59 +0100

    reverted not needed change

--- src/main/java/com/sda/App.java
+++ src/main/java/com/sda/App.java
@@ -6,7 +6,8 @@ package com.sda;
  */
 public class App 
 {
-    public static void main( String[] args ) {
+    public static void main( String[] args )
+    {
         System.out.println( "Hello World!" );
     }
 }
