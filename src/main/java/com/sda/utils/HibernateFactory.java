47e6aa6dacba9fc68fd4fff5b5a9a90752547997
Marcin Dolega <marcin92d@gmail.com>
2021-12-19 14:59:05 +0100

    Checkers created hibernate lol

---
+++ src/main/java/com/sda/utils/HibernateFactory.java
@@ -0,0 +1,26 @@
+package com.sda.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateFactory {
+    private Configuration getHibernateConfig() {
+        Configuration configuration = new Configuration();
+        configuration.setProperty("hibernate.connection.url", "jdbc:hsqldb:file:db-data/mydatabase;hsqldb.write_delay_millis=0");
+        configuration.setProperty("hibernate.connection.username", "admin123");
+        configuration.setProperty("hibernate.connection.password", "admin123");
+        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+        configuration.setProperty("hibernate.connection.driver_class", "org.hsqldb.jdbc.JDBCDriver");
+        configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+//		configuration.setProperty("hibernate.show_sql", "true");
+        return configuration;
+    }
+
+    public SessionFactory getSessionFactory() {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .applySettings(getHibernateConfig().getProperties()).build();
+        return getHibernateConfig().buildSessionFactory(registry);
+    }
+}
