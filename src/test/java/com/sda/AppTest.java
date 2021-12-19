34303b3d61e847b9f337042a4be89c344b4f2a7f
Adam Pusio <adam.pusio@outlook.com>
2021-12-19 09:55:05 +0100

    Initial commit

---
+++ src/test/java/com/sda/AppTest.java
@@ -0,0 +1,20 @@
+package com.sda;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assertions.assertTrue(false);
+    }
+}
