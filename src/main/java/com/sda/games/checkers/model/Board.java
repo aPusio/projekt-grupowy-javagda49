878ead7744e9f2c64031cc37a76c3d8260f291f1
Marcin Dolega <marcin92d@gmail.com>
2021-12-30 17:59:32 +0100

    Spot model created, board defined
    (cherry picked from commit eadf486b0f11944e9844a9c7ef02935d07f0df45)

--- src/main/java/com/sda/games/checkers/model/Board.java
+++ src/main/java/com/sda/games/checkers/model/Board.java
@@ -31,41 +31,41 @@ public class Board {
 
     public void resetBoard() {
         // initialize white pieces
-        boardSpots[0][0] = new Spot(0,0,new RegularPiece(true), true);
-        boardSpots[0][2] = new Spot(0,2,new RegularPiece(true), true);
-        boardSpots[0][4] = new Spot(0,4,new RegularPiece(true), true);
-        boardSpots[0][6] = new Spot(0,5,new RegularPiece(true), true);
-        boardSpots[1][1] = new Spot(1,1,new RegularPiece(true), true);
-        boardSpots[1][3] = new Spot(1,3,new RegularPiece(true), true);
-        boardSpots[1][5] = new Spot(1,5,new RegularPiece(true), true);
-        boardSpots[1][7] = new Spot(1,7,new RegularPiece(true), true);
-        boardSpots[2][0] = new Spot(2,0,new RegularPiece(true), true);
-        boardSpots[2][2] = new Spot(2,2,new RegularPiece(true), true);
-        boardSpots[2][4] = new Spot(2,4,new RegularPiece(true), true);
-        boardSpots[2][6] = new Spot(2,6,new RegularPiece(true), true);
+        boardSpots[0][0] = new Spot(0,0,new RegularPiece(true));
+        boardSpots[0][2] = new Spot(0,2,new RegularPiece(true));
+        boardSpots[0][4] = new Spot(0,4,new RegularPiece(true));
+        boardSpots[0][6] = new Spot(0,5,new RegularPiece(true));
+        boardSpots[1][1] = new Spot(1,1,new RegularPiece(true));
+        boardSpots[1][3] = new Spot(1,3,new RegularPiece(true));
+        boardSpots[1][5] = new Spot(1,5,new RegularPiece(true));
+        boardSpots[1][7] = new Spot(1,7,new RegularPiece(true));
+        boardSpots[2][0] = new Spot(2,0,new RegularPiece(true));
+        boardSpots[2][2] = new Spot(2,2,new RegularPiece(true));
+        boardSpots[2][4] = new Spot(2,4,new RegularPiece(true));
+        boardSpots[2][6] = new Spot(2,6,new RegularPiece(true));
 
         // initialize black pieces
-        boardSpots[5][1] = new Spot(5,1,new RegularPiece(false), true);
-        boardSpots[5][3] = new Spot(5,3,new RegularPiece(false), true);
-        boardSpots[5][5] = new Spot(5,5,new RegularPiece(false), true);
-        boardSpots[5][7] = new Spot(5,7,new RegularPiece(false), true);
-        boardSpots[6][0] = new Spot(6,0,new RegularPiece(false), true);
-        boardSpots[6][2] = new Spot(6,2,new RegularPiece(false), true);
-        boardSpots[6][4] = new Spot(6,4,new RegularPiece(false), true);
-        boardSpots[6][6] = new Spot(6,6,new RegularPiece(false), true);
-        boardSpots[7][1] = new Spot(7,1,new RegularPiece(false), true);
-        boardSpots[7][3] = new Spot(7,3,new RegularPiece(false), true);
-        boardSpots[7][5] = new Spot(7,5,new RegularPiece(false), true);
-        boardSpots[7][7] = new Spot(7,7,new RegularPiece(false), true);
+        boardSpots[5][1] = new Spot(5,1,new RegularPiece(false));
+        boardSpots[5][3] = new Spot(5,3,new RegularPiece(false));
+        boardSpots[5][5] = new Spot(5,5,new RegularPiece(false));
+        boardSpots[5][7] = new Spot(5,7,new RegularPiece(false));
+        boardSpots[6][0] = new Spot(6,0,new RegularPiece(false));
+        boardSpots[6][2] = new Spot(6,2,new RegularPiece(false));
+        boardSpots[6][4] = new Spot(6,4,new RegularPiece(false));
+        boardSpots[6][6] = new Spot(6,6,new RegularPiece(false));
+        boardSpots[7][1] = new Spot(7,1,new RegularPiece(false));
+        boardSpots[7][3] = new Spot(7,3,new RegularPiece(false));
+        boardSpots[7][5] = new Spot(7,5,new RegularPiece(false));
+        boardSpots[7][7] = new Spot(7,7,new RegularPiece(false));
 
         // initialize empty spots
-        boardSpots[3][1] = new Spot(3,1,null, false);
-        boardSpots[3][3] = new Spot(3,3,null, false);
-        boardSpots[3][5] = new Spot(3,5,null, false);
-        boardSpots[3][7] = new Spot(3,7,null, false);
-        boardSpots[4][0] = new Spot(4,0,null, false);
-        boardSpots[4][2] = new Spot(4,2,null, false);
-        boardSpots[4][4] = new Spot(4,4,null, false);
-        boardSpots[4][6] = new Spot(4,6,null, false);
+        boardSpots[3][1] = new Spot(3,1,null);
+        boardSpots[3][3] = new Spot(3,3,null);
+        boardSpots[3][5] = new Spot(3,5,null);
+        boardSpots[3][7] = new Spot(3,7,null);
+        boardSpots[4][0] = new Spot(4,0,null);
+        boardSpots[4][2] = new Spot(4,2,null);
+        boardSpots[4][4] = new Spot(4,4,null);
+        boardSpots[4][6] = new Spot(4,6,null);
     }
 }
