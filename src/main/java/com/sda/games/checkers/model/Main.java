e7d1c72f1cdcc60a99e6b5724ec726929e7be06f
borowcze <adamborowiec7@gmail.com>
2022-01-10 21:51:56 +0100

    adam commits

--- src/main/java/com/sda/games/checkers/model/Main.java
+++ src/main/java/com/sda/games/checkers/model/Main.java
@@ -11,8 +11,8 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-         Game game = new Game();
-         game.runGame();
+        Game game = new Game();
+        game.runGame();
 
     }
 }
