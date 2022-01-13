05fc8f1d3cf94eb41af07e1edc894186f977d170
Marcin Dolega <marcin92d@gmail.com>
2022-01-13 21:42:34 +0100

    Update - Adding players (id, name) to db started

--- src/main/java/com/sda/games/checkers/model/Main.java
+++
@@ -1,8 +0,0 @@
-package com.sda.games.checkers.model;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-}
--- src/main/java/com/sda/utils/HibernateFactory.java
+++ src/main/java/com/sda/utils/HibernateFactory.java
@@ -1,12 +1,5 @@
 package com.sda.utils;
 
-import com.sda.games.r_p_s.database.model.PlayerRPS;
-import com.sda.games.wheelOfFortune.model.Category;
-import com.sda.games.wheelOfFortune.model.Turn;
-import com.sda.games.wheelOfFortune.model.WheelOfFortune;
-import com.sda.games.wheelOfFortune.model.Word;
-import com.sda.games.rockPaperScissors.modelRPS.UserRPS;
-import com.sda.users.Player;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -21,13 +14,7 @@ public class HibernateFactory {
         configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
         configuration.setProperty("hibernate.connection.driver_class", "org.hsqldb.jdbc.JDBCDriver");
         configuration.setProperty("hibernate.hbm2ddl.auto", "update");
-        configuration.addAnnotatedClass(Category.class);
-        configuration.addAnnotatedClass(Turn.class);
-        configuration.addAnnotatedClass(WheelOfFortune.class);
-        configuration.addAnnotatedClass(Word.class);
-        configuration.addAnnotatedClass(Player.class);
-        configuration.addAnnotatedClass(UserRPS.class);
-        configuration.addAnnotatedClass(PlayerRPS.class);
+//		configuration.setProperty("hibernate.show_sql", "true");
         return configuration;
     }
 
