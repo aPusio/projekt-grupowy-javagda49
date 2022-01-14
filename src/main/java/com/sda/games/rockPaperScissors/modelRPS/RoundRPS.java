c90566be808096e1aa76e35a1d31b8b18f5452ed
TomekGin <tomek.ginszt@gmail.com>
2022-01-14 15:49:59 +0100

    ZAD 3.7 RPS Round model

--- src/main/java/com/sda/games/rockPaperScissors/modelRPS/RoundRPS.java
+++ src/main/java/com/sda/games/rockPaperScissors/modelRPS/RoundRPS.java
@@ -1,8 +1,21 @@
 package com.sda.games.rockPaperScissors.modelRPS;
 
+import lombok.*;
+
+import javax.persistence.OneToMany;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class RoundRPS {
     private Integer roundId;
+    private SymbolRPS symbol;
     private Integer symbol1Id;
     private Integer symbol2Id;
-    private SymbolRPS symbol;
+
+    @OneToMany
+    private SymbolRPS symbol1;
+    @OneToMany
+    private SymbolRPS symbol2;
 }
