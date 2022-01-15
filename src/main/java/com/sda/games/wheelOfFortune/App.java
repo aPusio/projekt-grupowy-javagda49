a43a1434f5355c0a62f87b2e2b4d37787729b40a
Anna Wittmann <annwitt@tlen.pl>
2022-01-15 16:00:19 +0100

    Wheel of fortune - WheelGame - początek mechaniki gry

--- src/main/java/com/sda/games/wheelOfFortune/App.java
+++ src/main/java/com/sda/games/wheelOfFortune/App.java
@@ -1,9 +1,35 @@
-package com.sda.games.wheelOfFortune;
+package com.sda.games.wheelOfFortune; //Tu piszemy jebany kod koła
 
-import java.sql.Connection;
+import com.sda.games.wheelOfFortune.methods.WheelGame;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("Start gry");
+        //wybierz kategorie
+        //losowanie w ramach kategori
+        String wylosowaneHaslo = WheelGame.idWord(4);
+        System.out.println(wylosowaneHaslo);
+        //char[] haslo=new char[wylosowaneHaslo.length()];
+        String[] haslo=wylosowaneHaslo.split("");
+        String[] hasloStan=new String[wylosowaneHaslo.length()];
 
+        for (int i=0;i<wylosowaneHaslo.length();i++) {
+            if (haslo[i].equals(" ")) {
+                hasloStan[i]=" ";
+            } else {
+                hasloStan[i]="_";
+            }
+        }
+
+        for (String string: hasloStan
+             ) {
+            System.out.print(string);
+            
+        }
+         
+
+  
+            
+        
     }
 }
---
+++ src/main/java/com/sda/games/wheelOfFortune/methods/WheelGame.java
@@ -0,0 +1,12 @@
+package com.sda.games.wheelOfFortune.methods;
+
+public class WheelGame {
+
+    public static String idWord(int nr) {
+        //tu będzie pobieranie po podanym ID słowo z bazy
+        String word="Dupa dupa całka z dupy";
+        return word;
+    }
+
+
+}
