db783f45adda64905cd2375e891305dfaed9bc34
Lukasz Malachowski <malachowski.lukasz@gmail.com>
2022-01-15 10:54:41 +0100

    Rock _Paper_Scissors 1.2 del csv class Add Game model

--- src/main/java/com/sda/games/rockPaperScissors/modelRPS/GameRPS.java
+++ src/main/java/com/sda/games/rockPaperScissors/modelRPS/GameRPS.java
@@ -1,4 +1,22 @@
 package com.sda.games.rockPaperScissors.modelRPS;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class GameRPS {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Integer id2;
+
 }
