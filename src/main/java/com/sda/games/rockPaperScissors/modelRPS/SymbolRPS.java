19591ed3a28440264b0447edcb99eb6c81a3818e
ohanke <steamoha@wp.pl>
2022-01-15 14:31:43 +0100

    symbol rps

--- src/main/java/com/sda/games/rockPaperScissors/modelRPS/SymbolRPS.java
+++ src/main/java/com/sda/games/rockPaperScissors/modelRPS/SymbolRPS.java
@@ -1,4 +1,12 @@
 package com.sda.games.rockPaperScissors.modelRPS;
 
+import java.util.*;
+
 public enum SymbolRPS {
+    ROCK,
+    PAPER,
+    SCISSORS;
+
+//    private List<Enum<SymbolRPS>> symbols = new ArrayList<>(Arrays.asList(SymbolRPS.values()));
+
 }
