a369bff6ba12dc8c172328e109ab3c8ea7686d6c
Mario77Gdansk <60828436+Mario77Gdansk@users.noreply.github.com>
2022-01-16 09:59:16 +0100

    ZAD-7-db corrections inside building proccess

--- src/main/java/com/sda/games/wheelOfFortune/model/WheelOfFortune.java
+++ src/main/java/com/sda/games/wheelOfFortune/model/WheelOfFortune.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import java.util.List;
 
@@ -14,6 +16,8 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 public class WheelOfFortune {
+    @Id
+    @GeneratedValue
     private Integer gameId;
     private Integer playerOneScore;
     private Integer playerTwoScore;
